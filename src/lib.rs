80acb099649105ea062bd6305c2a5bac2bc2d594
Jack Willis <jack@attac.us>
2022-04-04 20:03:02 -0500

    use enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,90 +1,112 @@
+#![allow(dead_code)]
+
 use num_rational::Rational32;
+use num_traits::{Pow, ToPrimitive, Zero};
 use std::fmt::Debug;
 
 const CENTS_PER_OCTAVE: f64 = 1_200.0;
 
-pub trait TuningInterval: Debug + PartialEq {
-    fn as_cents(&self) -> Cents;
-    fn as_ratio(&self) -> Ratio;
-
-    // fn add_cents(&self, cents: Cents) -> Box<dyn TuningInterval>;
-    // fn mul_ratio(&self, ratio: Ratio) -> Box<dyn TuningInterval>;
-
-    // fn add_octaves(&self, octaves: i32) -> Box<dyn TuningInterval> {
-    //     const CENTS_PER_OCTAVE: i32 = 1200;
-    //     self.add_cents(Cents((octaves * CENTS_PER_OCTAVE) as f64))
-    // }
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum TuningInterval {
+    Cents(f64),
+    Ratio(Rational32),
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Cents(f64);
-
-impl Cents {
-    pub fn new(cents: f64) -> Option<Self> {
+impl TuningInterval {
+    pub fn new_cents(cents: f64) -> Option<Self> {
         if cents.is_nan() || cents.is_infinite() {
             None
         } else {
-            Some(Cents(cents))
+            Some(Self::Cents(cents))
         }
     }
-}
 
-impl TuningInterval for Cents {
-    fn as_cents(&self) -> Cents {
-        *self
-    }
-
-    fn as_ratio(&self) -> Ratio {
-        //FIXME
-        Ratio::new(1, 1).unwrap()
-    }
-}
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Ratio(Rational32);
-
-impl Ratio {
-    pub fn new(numer: i32, denom: i32) -> Option<Self> {
-        if denom == 0 {
+    pub fn new_ratio(numer: i32, denom: i32) -> Option<Self> {
+        if denom.is_zero() {
             None
         } else {
-            Some(Self(Rational32::new(numer, denom)))
+            let ratio = Rational32::new(numer, denom);
+            Some(Self::Ratio(ratio))
+        }
+    }
+
+    pub fn to_cents(self) -> Self {
+        match self {
+            Self::Cents(cents) => Self::Cents(cents),
+            Self::Ratio(ratio) => {
+                let octaves = Self::Ratio(ratio).to_proportion().log2();
+                let cents = octaves * CENTS_PER_OCTAVE;
+                Self::Cents(cents)
+            }
+        }
+    }
+
+    pub fn to_ratio(self) -> Self {
+        match self {
+            Self::Cents(cents) => {
+                let proportion = Self::Cents(cents).to_proportion();
+                let approx_ratio = math::get_approximate_rational(proportion);
+                Self::Ratio(approx_ratio)
+            }
+            Self::Ratio(ratio) => Self::Ratio(ratio),
+        }
+    }
+
+    pub fn to_proportion(self) -> f64 {
+        match self {
+            Self::Cents(cents) => 2.0.pow(cents / CENTS_PER_OCTAVE),
+            Self::Ratio(ratio) => ratio.to_f64().unwrap(),
         }
     }
 }
 
-impl TuningInterval for Ratio {
-    fn as_cents(&self) -> Cents {
-        let ratio_as_double = num_traits::ToPrimitive::to_f64(&self.0).unwrap();
-        let octaves = ratio_as_double.log2();
-        Cents::new(octaves * CENTS_PER_OCTAVE).unwrap()
+mod math {
+    use num_rational::Rational32;
+
+    pub fn get_approximate_rational(x: f64) -> Rational32 {
+        farey(x - x.trunc()) + x.trunc() as i32
     }
 
-    fn as_ratio(&self) -> Ratio {
-        *self
-    }
-}
+    /// Farey algorithm: find closest rational to double in [0, 1]
+    fn farey(x: f64) -> Rational32 {
+        // See <http://www.johndcook.com/blog/2010/10/20/best-rational-approximation/>.
 
-#[cfg(test)]
-mod tests {
-    use crate::{Cents, TuningInterval};
+        const MAX_DENOM: i32 = 200;
 
-    #[test]
-    fn cents_must_be_sane_values() {
-        let xs = [f64::INFINITY, -f64::INFINITY, f64::NAN];
+        let mut a = 0;
+        let mut b = 1;
+        let mut c = 1;
+        let mut d = 1;
 
-        for x in xs {
-            assert!(Cents::new(x).is_none())
+        while b < MAX_DENOM && d <= MAX_DENOM {
+            let mediant = (a + c) as f64 / (b + d) as f64;
+
+            if almost_equal(x, mediant) {
+                if b + d <= MAX_DENOM {
+                    return Rational32::new(a + c, b + d);
+                } else if d > b {
+                    return Rational32::new(c, d);
+                } else {
+                    return Rational32::new(a, b);
+                }
+            } else if x > mediant {
+                a += c;
+                b += d;
+            } else {
+                c += a;
+                d += b;
+            }
+        }
+
+        if b < MAX_DENOM {
+            Rational32::new(c, d)
+        } else {
+            Rational32::new(a, b)
         }
     }
 
-    #[test]
-    fn cents_as_cents_is_identity() {
-        let xs = [0.0, -0.0, 0.7, 1.0, 3.14];
-
-        for x in xs {
-            let c = Cents::new(x).unwrap();
-            assert_eq!(c, c.as_cents());
-        }
+    fn almost_equal(x: f64, y: f64) -> bool {
+        let delta = (x - y).abs();
+        delta <= (f64::EPSILON * delta) || delta < f64::MIN_POSITIVE
     }
 }
